3e28d68b2dc7f0000fb93009c89c77f09535218e
balajixog <balaji31072005@gmail.com>
2026-06-27 00:24:47 +0530

    feat: initialize ESP32 firmware project

---
+++ firmware/firmware.ino
@@ -0,0 +1,17 @@
+// ---------- PIN DEFINITIONS ----------
+#define SOIL_PIN       34
+#define FLOW_PIN       18
+#define CURRENT_PIN    35
+#define RELAY_PIN      19
+
+// ---------- RELAY LOGIC ----------
+#define PUMP_ON   HIGH
+#define PUMP_OFF  LOW
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+
+}
